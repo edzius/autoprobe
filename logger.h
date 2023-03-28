5aa416539f11d8db50adb516136529337c073003
Edvinas Stunžėnas <edvinas@8devices.com>
2023-03-28 06:59:56 +0000

    Add compile-time switch for debug logging

--- logger.h
+++ logger.h
@@ -1,7 +1,11 @@
 #ifndef _LOGGER_H
 #define _LOGGER_H
 
+#ifdef DEBUG
 #define log_debug printf
+#else
+#define log_debug(...)
+#endif
 #define log_info printf
 #define log_notice printf
 #define log_warn printf
